5a179f31aac8be997f436652989a212eb0823b45
Your Name <you@example.com>
2018-04-18 21:52:39 +0800

    1

---
+++ src/main/java/com/example/demo/entity/SevProject.java
@@ -0,0 +1,110 @@
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Wicky
+ * @since 2018-04-18
+ */
+@TableName("sev_project")
+public class SevProject extends Model<SevProject> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "pro_id", type = IdType.AUTO)
+    private Integer proId;
+    @TableField("pro_name")
+    private String proName;
+    private Double price;
+    @TableField("ser_time")
+    private Double serTime;
+    @TableField("hig_price")
+    private Double higPrice;
+    @TableField("pri_price")
+    private Double priPrice;
+    private Integer typeid;
+
+
+    public Integer getProId() {
+        return proId;
+    }
+
+    public void setProId(Integer proId) {
+        this.proId = proId;
+    }
+
+    public String getProName() {
+        return proName;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getSerTime() {
+        return serTime;
+    }
+
+    public void setSerTime(Double serTime) {
+        this.serTime = serTime;
+    }
+
+    public Double getHigPrice() {
+        return higPrice;
+    }
+
+    public void setHigPrice(Double higPrice) {
+        this.higPrice = higPrice;
+    }
+
+    public Double getPriPrice() {
+        return priPrice;
+    }
+
+    public void setPriPrice(Double priPrice) {
+        this.priPrice = priPrice;
+    }
+
+    public Integer getTypeid() {
+        return typeid;
+    }
+
+    public void setTypeid(Integer typeid) {
+        this.typeid = typeid;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.proId;
+    }
+
+    @Override
+    public String toString() {
+        return "SevProject{" +
+        "proId=" + proId +
+        ", proName=" + proName +
+        ", price=" + price +
+        ", serTime=" + serTime +
+        ", higPrice=" + higPrice +
+        ", priPrice=" + priPrice +
+        ", typeid=" + typeid +
+        "}";
+    }
+}
---
+++ src/main/java/com/example/demo/web/TypeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Wicky
+ * @since 2018-04-18
+ */
+@Controller
+@RequestMapping("/demo/type")
+public class TypeController {
+
+}
+
